be0176cd8f7fa7c225439f535ee9e4891e76687f
LunaKaniz <147470436+LunaKaniz@users.noreply.github.com>
2024-08-26 22:36:18 +0800

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Layout from "./layout";
-import Index from "./pages";
+import Index from "./pages/Index";
 import Bmi from "./pages/Bmi";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
